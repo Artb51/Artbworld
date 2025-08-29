7ba3379d586f64dc044f88c25212966d20e76e21
Artb51 <Bhushanborkar786@gmail.com>
2025-08-29 15:20:12 +0530

    Create page.tsx
    Signed-off-by: Artb51 <Bhushanborkar786@gmail.com>

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,12 @@
+export default function Contact(){
+  return (
+    <div>
+      <h2 className="h1">Contact</h2>
+      <p><b>Company:</b> ARTB Contractors Pvt. Ltd.</p>
+      <p><b>Email:</b> support@artbcontractorsprivatelimited.com</p>
+      <p><b>Helpline:</b> +91-XXXXXXXXXX</p>
+      <p><b>Office:</b> Registered HQ (India)</p>
+      <p><b>Hours:</b> Mon–Sat, 10 AM – 6 PM</p>
+    </div>
+  )
+}
